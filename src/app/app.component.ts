86ee2bf2a577667c4cf949759a08ddba3948ba56
HersonCastillo <hersoncastillo12@hotmail.com>
2022-12-11 18:12:35 -0600

    set standalone configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'todolist';
-}
+export class AppComponent {}
---
+++ src/app/components/input-box/input-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-input-box',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './input-box.component.html',
+  styleUrls: ['./input-box.component.scss'],
+})
+export class InputBoxComponent {}
